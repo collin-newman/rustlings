4e13ef1ecce44b333ffc14042dba315dc5d62769
collin.newman <collin.newman37@gmail.com>
2023-10-25 16:09:34 -0400

    finished enums and strings

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut my_string = input.to_string();
+    my_string.push_str(" world!");
+    my_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut result = input.to_string().replace("cars", "balloons");
+    result
+
 }
 
 #[cfg(test)]
